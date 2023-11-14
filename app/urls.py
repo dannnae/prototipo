724e4827cbaa51c60fc682bc055b9f217bdfc194
dannnae <dan.venegas@duocuc.cl>
2023-11-13 23:40:31 -0300

    urls para login y register

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.login_paciente, name='login'),
     path('cita', views.cita, name='cita'),
     path('agendar', views.agendar, name='agendar'),
     path('index', views.index, name='index'),
+    path('registro_paciente', views.register_paciente, name='registro_paciente'),
 ]
