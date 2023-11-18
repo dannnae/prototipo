4359b740a7ac7a8017d076bebfd9e1e0ee7c2e17
dannnae <dan.venegas@duocuc.cl>
2023-11-17 21:38:46 -0300

    urls y views crud citas

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('login', views.login_paciente, name='login'),
-    path('cita', views.cita, name='cita'),
-    path('agendar', views.agendar, name='agendar'),
     path('index', views.index, name='index'),
     path('registro_paciente', views.register_paciente, name='registro_paciente'),
-    path('paciente_cuenta', views.paciente_cuenta, name='paciente_cuenta'),
+    path('paciente_cuenta', views.CitaListView.as_view(), name='paciente_cuenta'),
     path('logout_paciente', views.logout_paciente, name='logout_paciente'),
+
+    path('citas/nueva/', views.CitaCreateView.as_view(), name='cita_create'),
+    path('citas/<int:pk>/editar/', views.CitaUpdateView.as_view(), name='cita_update'),
+    path('citas/<int:pk>/eliminar/', views.CitaDeleteView.as_view(), name='cita_delete'),
 ]
